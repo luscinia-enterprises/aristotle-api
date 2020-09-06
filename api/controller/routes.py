de4ae682bef99d253c437faa49d27b9c1bb12009
Jacob Chun <jacob.chun@gmail.com>
2020-09-06 18:36:21 -0400

    Update routes.py

--- api/controller/routes.py
+++ api/controller/routes.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .views import *
+from .endpoints import *
 
 
 def initialize_routes(api):
